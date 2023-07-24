c2a8d283d7a06978407f7a4e6e70056054f4c41a
Yu Zhou Zhang <yuzspanish@gmail.com>
2023-07-24 18:10:23 +0100

    complete clicking and keyboard functionality

---
+++ README.md
@@ -0,0 +1,4 @@
+# Odin Calculator Project
+
+This is a calculator built purely using HTML/CSS/JavaScript.
+It was satisfying putting keyboard event listeners onto it and marvelling at the instant response times.
--- script.js
+++ script.js
@@ -1,21 +1,60 @@
-const display = document.querySelector("#display");
+const displayCurrent = document.querySelector("#display-current");
+const displayPrevious = document.querySelector("#display-previous");
 
 const buttons = document.querySelectorAll(".button");
-
-function addNumberToDisplay(number) {
-  if (display.textContent === "0") {
-    display.textContent = number;
-  } else {
-    display.textContent += number;
-  }
-}
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.classList.contains("number")) {
       addNumberToDisplay(button.textContent);
+    } else if (button.classList.contains("operator")) {
+      onOperatorClick(button.textContent);
+    } else if (button.id === "clear") {
+      clearDisplay();
+    } else if (button.id === "equals") {
+      onEqualsClick();
+    } else if (button.id === "delete") {
+      deleteLastCharacter();
     }
   });
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Backspace") {
+    deleteLastCharacter();
+  }
+  if (e.key === "Escape") {
+    clearDisplay();
+  }
+  if (e.key === "Enter") {
+    onEqualsClick();
+  }
+  if (e.key === ".") {
+    addNumberToDisplay(".");
+  }
+  if (
+    e.key === "+" ||
+    e.key === "-" ||
+    e.key === "*" ||
+    e.key === "/" ||
+    e.key === "%"
+  ) {
+    onOperatorClick(e.key);
+  }
+  if (
+    e.key === "0" ||
+    e.key === "1" ||
+    e.key === "2" ||
+    e.key === "3" ||
+    e.key === "4" ||
+    e.key === "5" ||
+    e.key === "6" ||
+    e.key === "7" ||
+    e.key === "8" ||
+    e.key === "9"
+  ) {
+    addNumberToDisplay(e.key);
+  }
 });
 
 function add(x, y) {
@@ -33,6 +72,9 @@ function divide(x, y) {
   }
   return x / y;
 }
+function modulo(x, y) {
+  return x % y;
+}
 
 function operate(operator, x, y) {
   switch (operator) {
@@ -44,7 +86,68 @@ function operate(operator, x, y) {
       return multiply(x, y);
     case "/":
       return divide(x, y);
+    case "%":
+      return modulo(x, y);
     default:
       throw new Error("Invalid operator!");
   }
+}
+function validateResult(result) {
+  if (result > 999999999999) {
+    clearDisplay();
+    return "Number too large!";
+  }
+  if (result === NaN) {
+    clearDisplay();
+    return "Invalid operation!";
+  }
+  if (toString(result).length > 12) {
+    return Math.round(result * 1000000000000) / 1000000000000;
+  }
+
+  return result;
+}
+function addNumberToDisplay(number) {
+  if (displayCurrent.textContent.length > 12) {
+    return;
+  }
+  if (displayCurrent.textContent === "0") {
+    number === "."
+      ? (displayCurrent.textContent = "0.")
+      : (displayCurrent.textContent = number);
+  } else {
+    if (number === "." && displayCurrent.textContent.includes(".")) {
+      return;
+    }
+    displayCurrent.textContent += number;
+  }
+}
+
+function clearDisplay() {
+  displayCurrent.textContent = "0";
+  displayPrevious.textContent = "";
+}
+
+function deleteLastCharacter() {
+  if (displayCurrent.textContent.length === 1) {
+    displayCurrent.textContent = "0";
+    return;
+  }
+  displayCurrent.textContent = displayCurrent.textContent.slice(0, -1);
+}
+
+let firstOperand = null;
+
+function onOperatorClick(operator) {
+  firstOperand = Number(displayCurrent.textContent);
+  clearDisplay();
+  displayPrevious.textContent = `${firstOperand} ${operator}`;
+}
+function onEqualsClick() {
+  const secondOperand = Number(displayCurrent.textContent);
+  const operator = displayPrevious.textContent.slice(-1);
+  const result = operate(operator, firstOperand, secondOperand);
+
+  displayCurrent.textContent = validateResult(result);
+  displayPrevious.textContent = `${firstOperand} ${operator} ${secondOperand} =`;
 }
